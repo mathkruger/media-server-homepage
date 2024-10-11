22e4537eebc3e7fe5b26869cab125dd5331f0a5b
Matheus Kruger <ms-kruger@hotmail.com>
2024-10-11 10:06:26 -0300

    removendo um teste

--- src/index.mjs
+++ src/index.mjs
@@ -3,14 +3,8 @@ import * as http from "node:http";
 import { handleRoute } from "./core/handle-route.mjs";
 import { routes } from "./routes.mjs";
 import { MIME_TYPES } from "./consts/mime-types.mjs";
-import { createUser } from "./services/users.mjs";
 
 const PORT = Number(process.argv[2] || "8088");
-const seed = process.argv[3];
-
-if (seed) {
-  createUser({ username: 'admin', password: 'admin' }).then(() => console.log("user created <3"));
-}
 
 http
   .createServer(async (req, res) => {
